ab884ca137ad714cb515e17014850e0e3f696f91
Colin Craft <colin@colincraft.com>
2015-01-27 18:44:54 -0800

    More updates

--- learnjquery4/app.js
+++ learnjquery4/app.js
@@ -1,14 +1,27 @@
 $(document).ready(function(){
 
 $("form").append("<span>That password is less than 6 characters!</span>");
-$("form").on("submit", function(e){
-	e.preventDefault();
-	});
-$("form").on("submit", function(e){
+
+$("form").on("keyup", function(e){
 	if ($("input[type=password").val().length >= 6){
 		console.log("more than 6");
-		$("span").remove();
+		$("span").addClass("visibilty");
+		$('form').attr('id', 'canSubmit');
+		return true;
+	}
+	else {
+		$("span").removeClass("visibilty");
+		$('form').attr('id', 'cannotSubmit');
+	}
+});
+$('form').on("submit", function(e){
+	if($('form').attr("id") === "canSubmit"){
+		console.log("show success");
+		$("p").removeClass("visibilty");
 
+	}
+	else {
+		console.log("show no success");
 	}
 });
 
